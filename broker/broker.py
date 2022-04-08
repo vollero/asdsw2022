11b9eb08e6e5a4d3d16a3f4a9e4245a8d25a68d4
vollero <vollero@gmail.com>
2022-04-08 11:00:49 +0000

    added send, improved subscribe & unsubscribe

--- broker/broker.py
+++ broker/broker.py
@@ -2,6 +2,7 @@ import socket
 from sys import argv
 import re
 from threading import Thread, Lock
+import json
 
 def connection_manager_thread(id_, conn):
     global activeConnections
@@ -11,6 +12,7 @@ def connection_manager_thread(id_, conn):
     connected = False
     print('Client: {}'.format(id_))
     regexTOPIC = r"\{\"topic\":[\ ]\"([a-zA-Z0-9]+)\"\}"
+    regexJSON = r"(\{[\"a-zA-Z0-9\,\ \:\"]+\})"
     
     while not connected:
         data = conn.recv(1024)
@@ -36,31 +38,78 @@ def connection_manager_thread(id_, conn):
             connected = False
 
         if bool(re.search('^\[SUBSCRIBE\]', data.decode('utf-8'))):
-            topic = re.findall(regexTOPIC, data.decode('utf-8'))[0]
+            stringa = re.findall(regexJSON, data.decode('utf-8'))[0]
+            parametri = json.loads(stringa)
+            '''
+            if "topic" in parametri:
+                pass
+            if "topics" in parametri:
+                pass
+            '''
+            topic = parametri["topic"]
+            
             # logica di sottoscrizione
+            mutexACs.acquire()
+            activeConnections[id_]["topics"].add(topic)
+            mutexACs.release()
+
             mutexTOPICs.acquire()
             if not topic in topics:
                 topics[topic] = set()    
             topics[topic].add(id_)
             mutexTOPICs.release()
             print(topics)
+            
             # notifica utente avvenuta sottoscrizione
             response = 'Sottoscritto al topic: {}\n'.format(topic)
             conn.sendall(response.encode())
 
         if bool(re.search('^\[UNSUBSCRIBE\]', data.decode('utf-8'))):
-            topic = re.findall(regexTOPIC, data.decode('utf-8'))[0]
+            stringa = re.findall(regexJSON, data.decode('utf-8'))[0]
+            parametri = json.loads(stringa)
+            topic = parametri["topic"]
+
             # logica di sottoscrizione
+
+            mutexACs.acquire()
+            activeConnections[id_]["topics"].remove(topic)
+            mutexACs.release()
+
             mutexTOPICs.acquire()
             if topic in topics:
                 if id_ in topics[topic]:
-                    topics[topic].remove(id_)   
+                    topics[topic].remove(id_)
+                if len(topics[topic]) == 0:
+                    del topics[topic]
             mutexTOPICs.release()
             print(topics)
             # notifica utente avvenuta sottoscrizione
             response = 'Cancellazione della sottoscrizione al topic: {}\n'.format(topic)
             conn.sendall(response.encode())
 
+        if bool(re.search('^\[SEND\]', data.decode('utf-8'))):
+            stringa = re.findall(regexJSON, data.decode('utf-8'))[0]
+            parametri = json.loads(stringa)
+            topic = parametri["topic"]
+            message = parametri["message"]
+
+            # logica di invio del messaggio
+            risposta = dict()
+            risposta["id"] = id_
+            risposta["topic"] = topic
+            risposta["messaggio"] = message
+
+            stringa = json.dumps(risposta)
+            print(stringa)
+
+            mutexACs.acquire()
+            mutexTOPICs.acquire()
+            subscribers = topics[topic]
+            for subID in subscribers:
+                recv_conn = activeConnections[subID]["connessione"]
+                recv_conn.sendall(stringa.encode())
+            mutexTOPICs.release()
+            mutexACs.release()
 
         print('{}: chat message: {}'.format(id_, data[:-1].decode('utf-8')))
     
@@ -99,7 +148,8 @@ if __name__ == '__main__':
                     'address': addr,
                     'connessione': conn,
                     'connected': False,
-                    'id': curr_id + 1
+                    'id': curr_id + 1,
+                    'topics': set()
                     }
             curr_id += 1
             mutexACs.release()
