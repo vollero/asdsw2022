1947c0d06f3168c18440b3e8d62d288793f6ae16
vollero <vollero@gmail.com>
2022-04-11 13:30:41 +0000

    global refactoring

--- broker/broker.py
+++ broker/broker.py
@@ -4,9 +4,9 @@ import re
 from threading import Thread, Lock
 import json
 
-def decodeCommand(message):
+def decodeCommand(message, stato):
     regexCOMMAND = r'^\[([A-Z]+)\]'
-    regexJSON = r"(\{[\"a-zA-Z0-9\,\ \:\"]+\})"
+    regexJSON = r"(\{[\"a-zA-Z0-9\,\ \:\"\]\[]+\})"
 
     withArgs = {"SUBSCRIBE", "UNSUBSCRIBE", "SEND"}    
 
@@ -16,52 +16,62 @@ def decodeCommand(message):
     if command:
         comando = dict()
         comando['azione'] = command
-        if command in withArgs:
+        if command in withArgs and stato == "CONNESSO":
             stringa = re.findall(regexJSON, message)[0]
             parametri = json.loads(stringa)
             comando['parametri'] = parametri
 
     return comando
 
-def connection_manager_thread(id_, conn):
+def updateState(id_, stato, comando):
     global activeConnections
     global mutexACs 
-    global mutexTOPICs
-    global topics
-    connected = False
-    print('Client: {}'.format(id_))
-    
-    while not connected:
-        data = conn.recv(1024)
-        comando = decodeCommand(data.decode('utf-8'))
+
+    if stato == "PRE-CONNESSIONE":
         if comando['azione'] == "CONNECT":
-            connected = True
+            newStato = "CONNESSO"
+            
             mutexACs.acquire()
             activeConnections[id_]['connected'] = True
             mutexACs.release()
             print('{} connected!'.format(id_))
 
-    data = '{}\n'.format(id_)
-    conn.sendall(data.encode())
+            return newStato
 
-    mutexACs.acquire()
-    print(activeConnections)
-    mutexACs.release()
-
-    while connected:
-        data = conn.recv(1024)
-        if not data:
-            break
-
-        comando = decodeCommand(data.decode('utf-8'))
-
+    if stato == "CONNESSO":
         if comando['azione'] == "DISCONNECT":
-            connected = False
+            newStato = "IN-USCITA"
+            return newStato
 
-        if comando['azione'] == "SUBSCRIBE":
-            topic = comando['parametri']['topic']
+    return stato
+
+def subscribe(id_, conn, comando):
+    global activeConnections
+    global mutexACs 
+    global mutexTOPICs
+    global topics
+
+    if "topic" in comando['parametri']:
+
+        topic = comando['parametri']['topic']
             
-            # logica di sottoscrizione
+        mutexACs.acquire()
+        activeConnections[id_]["topics"].add(topic)
+        mutexACs.release()
+
+        mutexTOPICs.acquire()
+        if not topic in topics:
+            topics[topic] = set()    
+        topics[topic].add(id_)
+        mutexTOPICs.release()
+        print(topics)
+            
+        response = 'Sottoscritto al topic: {}\n'.format(topic)
+        conn.sendall(response.encode())
+
+    if "topics" in comando['parametri']:
+        for topic in comando['parametri']['topics']:
+
             mutexACs.acquire()
             activeConnections[id_]["topics"].add(topic)
             mutexACs.release()
@@ -73,54 +83,65 @@ def connection_manager_thread(id_, conn):
             mutexTOPICs.release()
             print(topics)
             
-            # notifica utente avvenuta sottoscrizione
             response = 'Sottoscritto al topic: {}\n'.format(topic)
             conn.sendall(response.encode())
 
-        if comando['azione'] == "UNSUBSCRIBE":
-            topic = comando['parametri']['topic']
+def unsubscribe(id_, conn, comando):
+    global activeConnections
+    global mutexACs 
+    global mutexTOPICs
+    global topics
 
-            # logica di sottoscrizione
-            mutexACs.acquire()
-            activeConnections[id_]["topics"].remove(topic)
-            mutexACs.release()
+    topic = comando['parametri']['topic']
 
-            mutexTOPICs.acquire()
-            if topic in topics:
-                if id_ in topics[topic]:
-                    topics[topic].remove(id_)
-                if len(topics[topic]) == 0:
-                    del topics[topic]
-            mutexTOPICs.release()
-            print(topics)
-            # notifica utente avvenuta sottoscrizione
-            response = 'Cancellazione della sottoscrizione al topic: {}\n'.format(topic)
-            conn.sendall(response.encode())
+    mutexACs.acquire()
+    activeConnections[id_]["topics"].remove(topic)
+    mutexACs.release()
 
-        if comando['azione'] == "SEND":
-            topic = comando["parametri"]["topic"]
-            message = comando["parametri"]["message"]
-
-            # logica di invio del messaggio
-            risposta = dict()
-            risposta["id"] = id_
-            risposta["topic"] = topic
-            risposta["messaggio"] = message
-
-            stringa = json.dumps(risposta) + '\n'
-            print(stringa)
-
-            mutexACs.acquire()
-            mutexTOPICs.acquire()
-            subscribers = topics[topic]
-            for subID in subscribers:
-                recv_conn = activeConnections[subID]["connessione"]
-                recv_conn.sendall(stringa.encode())
-            mutexTOPICs.release()
-            mutexACs.release()
-
-        print('{}: chat message: {}'.format(id_, data[:-1].decode('utf-8')))
+    mutexTOPICs.acquire()
+    if topic in topics:
+        if id_ in topics[topic]:
+            topics[topic].remove(id_)
+        if len(topics[topic]) == 0:
+            del topics[topic]
+    mutexTOPICs.release()
+    print(topics)
     
+    response = 'Cancellazione della sottoscrizione al topic: {}\n'.format(topic)
+    conn.sendall(response.encode())
+
+def send(id_, conn, comando):
+    global activeConnections
+    global mutexACs 
+    global mutexTOPICs
+    global topics
+
+    topic = comando["parametri"]["topic"]
+    message = comando["parametri"]["message"]
+
+    risposta = dict()
+    risposta["id"] = id_
+    risposta["topic"] = topic
+    risposta["messaggio"] = message
+
+    stringa = json.dumps(risposta) + '\n'
+    print(stringa)
+
+    mutexACs.acquire()
+    mutexTOPICs.acquire()
+    subscribers = topics[topic]
+    for subID in subscribers:
+        recv_conn = activeConnections[subID]["connessione"]
+        recv_conn.sendall(stringa.encode())
+    mutexTOPICs.release()
+    mutexACs.release()
+
+def disconnect(id_):
+    global activeConnections
+    global mutexACs 
+    global mutexTOPICs
+    global topics
+
     mutexACs.acquire()
     curr_topics = activeConnections[id_]["topics"]    
     mutexACs.release()
@@ -137,6 +158,34 @@ def connection_manager_thread(id_, conn):
     mutexACs.release()
     conn.close()
 
+def applyCommand(id_, conn, comando, stato):
+    if (stato == "CONNESSO"):
+        if comando['azione'] == "SUBSCRIBE":
+            subscribe(id_, conn, comando)
+            return True
+        if comando['azione'] == "UNSUBSCRIBE":
+            unsubscribe(id_, conn, comando)
+            return True
+        if comando['azione'] == "SEND":
+            send(id_, conn, comando)
+            return True
+        if comando['azione'] == "DISCONNECT":
+            disconnect(id_)
+            return True
+    return False
+
+def connection_manager_thread(id_, conn):
+    stato = "PRE-CONNESSIONE"               # CONNESSO, IN-USCITA
+    print('Client: {}'.format(id_))
+
+    while not (stato == "IN-USCITA"):
+        data = conn.recv(1024)
+        if not data:
+            break
+        comando = decodeCommand(data.decode('utf-8'), stato)
+        applyCommand(id_, conn, comando, stato)
+        stato = updateState(id_, stato, comando)
+    
 if __name__ == '__main__':
 
     localIP     = argv[1]
